d2cbc3aa2c235eba84ef66bf89236f6aaf2ff3c5
Samir Talwar <samir@noodlesandwich.com>
2016-12-10 00:20:30 +0000

    Day 1, Exercise 2.

--- AOC1.hs
+++ AOC1.hs
@@ -1,4 +1,5 @@
 import qualified Data.Char as Char
+import qualified Data.Set as Set
 
 data Movement = Forward | RotateLeft | RotateRight
   deriving (Eq, Show)
@@ -9,7 +10,8 @@ data Direction = North | East | South | West
 main = do
   contents <- getContents
   let movements = decode contents
-  let destination = foldr move (0, 0, North) movements
+  let locations = moveLots (0, 0, North) [(0, 0)] movements
+  let destination = firstVisitedTwice locations
   let blocks = distance destination
   putStrLn $ show blocks
 
@@ -22,17 +24,28 @@ decodeForward (char : rest) n
   | Char.isDigit char = decodeForward rest (n * 10 + Char.digitToInt char)
   | otherwise = replicate n Forward ++ decode rest
 
-move RotateLeft (x, y, North) = (x, y, West)
-move RotateLeft (x, y, West) = (x, y, South)
-move RotateLeft (x, y, South) = (x, y, East)
-move RotateLeft (x, y, East) = (x, y, North)
-move RotateRight (x, y, North) = (x, y, East)
-move RotateRight (x, y, East) = (x, y, South)
-move RotateRight (x, y, South) = (x, y, West)
-move RotateRight (x, y, West) = (x, y, North)
-move Forward (x, y, North) = (x, y + 1, North)
-move Forward (x, y, East) = (x + 1, y, East)
-move Forward (x, y, South) = (x, y - 1, South)
-move Forward (x, y, West) = (x - 1, y, West)
+moveLots position locations [] = locations
+moveLots position locations (movement : rest) =
+  let (newPosition, newLocations) = move movement position
+  in moveLots newPosition (locations ++ newLocations) rest
 
-distance (x, y, _) = abs x + abs y
+move RotateLeft  (x, y, North) = ((x, y, West ), [])
+move RotateLeft  (x, y, West ) = ((x, y, South), [])
+move RotateLeft  (x, y, South) = ((x, y, East ), [])
+move RotateLeft  (x, y, East ) = ((x, y, North), [])
+move RotateRight (x, y, North) = ((x, y, East ), [])
+move RotateRight (x, y, East ) = ((x, y, South), [])
+move RotateRight (x, y, South) = ((x, y, West ), [])
+move RotateRight (x, y, West ) = ((x, y, North), [])
+move Forward     (x, y, North) = ((x, y + 1, North), [(x, y + 1)])
+move Forward     (x, y, East ) = ((x + 1, y, East ), [(x + 1, y)])
+move Forward     (x, y, South) = ((x, y - 1, South), [(x, y - 1)])
+move Forward     (x, y, West ) = ((x - 1, y, West ), [(x - 1, y)])
+
+firstVisitedTwice locations = firstVisitedTwice' locations Set.empty
+  where
+  firstVisitedTwice' (location : rest) visited
+    | location `Set.member` visited = location
+    | otherwise = firstVisitedTwice' rest (Set.insert location visited)
+
+distance (x, y) = abs x + abs y
