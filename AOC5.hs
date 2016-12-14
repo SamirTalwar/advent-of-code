d3ecb3299fe597a1f74f91e26fde81661b45394d
Samir Talwar <samir@noodlesandwich.com>
2016-12-14 00:46:09 +0000

    Day 5, Exercise 2.

--- AOC5.hs
+++ AOC5.hs
@@ -3,9 +3,12 @@
 import           Control.Monad (mapM_)
 import qualified Crypto.Hash
 import           Data.ByteString (ByteString)
+import qualified Data.List as List
+import qualified Data.Maybe as Maybe
 import qualified Data.Text as Text
 import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import qualified Data.Text.IO as IO
+import qualified Numeric
 
 main = do
   doorId <- Text.strip <$> IO.getContents
@@ -15,9 +18,12 @@ crackPassword doorId =
   [0..]
     |> map (\i -> hashText (doorId `Text.append` (Text.pack $ show i)))
     |> filter (\digest -> Text.take 5 digest == "00000")
-    |> map (\digest -> Text.index digest 5)
-    |> take 8
+    |> map (\digest -> (fst $ head $ Numeric.readHex [Text.index digest 5], Text.index digest 6))
+    |> assemble 8
     |> Text.pack
+
+assemble n characters =
+  map (\position -> snd $ Maybe.fromJust $ List.find (\(p, _) -> position == p) characters) [0..(n - 1)]
 
 hashText :: Text.Text -> Text.Text
 hashText text = Text.pack $ show $ md5 $ encodeUtf8 text
