8139e7461837bdae34d512992b36682721b43d86
Samir Talwar <samir@noodlesandwich.com>
2016-12-19 11:48:40 +0000

    Day 19, Exercise 1.

---
+++ AOC_19.hs
@@ -0,0 +1,12 @@
+import Data.Sequence as Seq
+
+main = do
+  count <- read <$> getContents
+  print $ winner count
+
+winner :: Int -> Int
+winner count = winner' $ Seq.fromList [1..count]
+  where
+  winner' elves
+    | Seq.length elves == 1 = elves `index` 0
+    | otherwise = winner' (Seq.drop 2 elves |> elves `index` 0)
