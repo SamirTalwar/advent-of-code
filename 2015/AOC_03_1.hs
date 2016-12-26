100e08150404414d6cdd4bfa0517df8f11e446c2
Samir Talwar <samir@noodlesandwich.com>
2016-12-26 12:31:11 +0000

    2015: Day 3, Exercise 1.

---
+++ 2015/AOC_03_1.hs
@@ -0,0 +1,29 @@
+import qualified Data.Char as Char
+import qualified Data.Set as Set
+
+data Direction = North | East | South | West
+  deriving (Eq, Show)
+
+type Coordinates = (Int, Int)
+
+main = do
+  directions <- parseInput <$> trim <$> getContents
+  let visitedLocations = Set.fromList $ scanl (flip apply) (0, 0) directions
+  print $ Set.size visitedLocations
+
+trim :: String -> String
+trim = takeWhile (not . Char.isSpace) . dropWhile Char.isSpace
+
+parseInput :: String -> [Direction]
+parseInput = map parseChar
+  where
+  parseChar '^' = North
+  parseChar '>' = East
+  parseChar 'v' = South
+  parseChar '<' = West
+
+apply :: Direction -> Coordinates -> Coordinates
+apply North (x, y) = (x, y + 1)
+apply East (x, y) = (x + 1, y)
+apply South (x, y) = (x, y - 1)
+apply West (x, y) = (x - 1, y)
