607771fef713782397e4990f76fb5073ca8573c0
Samir Talwar <samir@noodlesandwich.com>
2016-12-27 00:34:56 +0000

    2015: Day 5, Exercise 1.

---
+++ 2015/AOC_05_1.hs
@@ -0,0 +1,19 @@
+main = do
+  input <- lines <$> getContents
+  let nice = filter isNice input
+  print $ length nice
+
+isNice :: String -> Bool
+isNice string = hasThreeVowels && containsDoubleLetter && doesNotContainNaughtyTuplets
+  where
+  hasThreeVowels = length (filter (`elem` vowels) string) >= 3
+  containsDoubleLetter = any (uncurry (==)) $ stringInPairs
+  doesNotContainNaughtyTuplets = not $ any (`elem` naughtyTuplets) $ stringInPairs
+  stringInPairs = pairs string
+  vowels = "aeiou"
+  naughtyTuplets = [('a', 'b'), ('c', 'd'), ('p', 'q'), ('x', 'y')]
+
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs [x] = []
+pairs (a : xs@(b : _)) = (a, b) : pairs xs
