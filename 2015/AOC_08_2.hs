12e31b9719ca2480f5f380945cf575b50897af12
Samir Talwar <samir@noodlesandwich.com>
2016-12-27 02:57:00 +0000

    2015: Day 8, Exercise 2.

---
+++ 2015/AOC_08_2.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Char as Char
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as IO
+import qualified Numeric
+
+main = do
+  strings <- Text.lines <$> IO.getContents
+  let stringLiterals = map encode strings
+  let sizeOfStringLiterals = sum $ map Text.length stringLiterals
+  let sizeOfStrings = sum $ map Text.length strings
+  print sizeOfStringLiterals
+  print sizeOfStrings
+  print $ sizeOfStringLiterals - sizeOfStrings
+
+encode :: Text -> Text
+encode string = Text.concat ["\"", Text.concatMap encodeChar string, "\""]
+  where
+  encodeChar '\\' = "\\\\"
+  encodeChar '"' = "\\\""
+  encodeChar char = Text.singleton char
