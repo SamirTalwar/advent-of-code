f7490268d7d2562cc7821215785677bae66f3548
Samir Talwar <samir@noodlesandwich.com>
2016-12-28 00:25:29 +0000

    2015: Day 17, Exercise 2.

---
+++ 2015/AOC_17_2.hs
@@ -0,0 +1,10 @@
+import qualified Data.List as List
+
+main = do
+  containerSizes <- map read <$> lines <$> getContents
+  let containerCombinations = map (\k -> combinations k containerSizes) [0..length containerSizes]
+  print $ head $ dropWhile (== 0) $ map (\combinations -> length $ filter (== 150) $ map sum combinations) containerCombinations
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n list = [x : xs | x : ts <- List.tails list, xs <- combinations (n - 1) ts]
