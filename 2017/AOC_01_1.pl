90fcab3162d45d132fba2fe78866523e223c4f70
Samir Talwar <samir@noodlesandwich.com>
2017-12-03 15:11:59 +0000

    2017: Day 1, Exercise 1.

--- 2017/AOC_01_1.pl
+++ 2017/AOC_01_1.pl
@@ -3,5 +3,22 @@
 main :-
   current_input(S),
   read_numbers(S, Ns),
-  print(Ns),
-  format("\n", []).
+  [Head | _] = Ns,
+  append(Ns, [Head], CycledNs),
+  matches_next(CycledNs, Matches),
+  sum(Matches, Sum),
+  format("~d\n", [Sum]).
+
+matches_next(Ns, Matches) :- matches_next(Ns, [], Matches).
+matches_next([], Matches, Matches).
+matches_next([_], Matches, Matches).
+matches_next([A, A | Rest], Matches, Output) :-
+  matches_next([A | Rest], [A | Matches], Output).
+matches_next([_, B | Rest], Matches, Output) :-
+  matches_next([B | Rest], Matches, Output).
+
+sum(Values, Result) :- sum(Values, 0, Result).
+sum([], Result, Result).
+sum([V | Vs], Accumulator, Result) :-
+  Next is Accumulator + V,
+  sum(Vs, Next, Result).
