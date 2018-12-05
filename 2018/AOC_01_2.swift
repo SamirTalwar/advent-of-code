e25f4cc8dbe5037e12171195259a8c1571399d75
Samir Talwar <samir@noodlesandwich.com>
2018-12-05 12:22:57 +0000

    2018: Day 1, Exercise 2.

---
+++ 2018/AOC_01_2.swift
@@ -0,0 +1,12 @@
+func main() {
+    let numbers = StdIn().map({ Int($0)! })
+    var seen: Set<Int> = []
+    var i = 0
+    var current = 0
+    while !seen.contains(current) {
+        seen.insert(current)
+        current += numbers[i]
+        i = (i + 1) % numbers.count
+    }
+    print(current)
+}
