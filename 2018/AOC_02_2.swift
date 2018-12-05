2ed2c94a44d8419ec9bd82b36e823e28128ec014
Samir Talwar <samir@noodlesandwich.com>
2018-12-05 13:00:36 +0000

    2018: Day 2, Exercise 2.

---
+++ 2018/AOC_02_2.swift
@@ -0,0 +1,18 @@
+func main() {
+    let ids = Array(StdIn())
+    let distances = ids.enumerated()
+        .flatMap({ a in ids.dropFirst(a.offset)
+                .map({ b in distanceBetween(a.element, b) }) })
+    guard let (lettersInCommon, _) = distances.first(where: { _, distance in distance == 1 }) else {
+        fatalError("Could not find two IDs with a distance of 1.")
+    }
+    print(lettersInCommon)
+}
+
+func distanceBetween(_ a: String, _ b: String) -> (String, Int) {
+    if a.count != b.count {
+        fatalError("The two strings are not the same length")
+    }
+    let lettersInCommon = String(zip(a, b).filter { x, y in x == y }.map { x, _ in x })
+    return (lettersInCommon, a.count - lettersInCommon.count)
+}
