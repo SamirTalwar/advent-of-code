74b3ae852c752c4c78f31f1301437cec1f63abb8
Samir Talwar <samir@noodlesandwich.com>
2018-12-05 17:31:18 +0000

    2018: Day 4, Exercise 2.

---
+++ 2018/AOC_04_2.swift
@@ -0,0 +1,158 @@
+typealias GuardId = Int
+
+struct Date: Comparable, Hashable {
+    let year: Int
+    let month: Int
+    let day: Int
+
+    static func == (lhs: Date, rhs: Date) -> Bool {
+        return lhs.year == rhs.year
+            && lhs.month == rhs.month
+            && lhs.day == rhs.day
+    }
+
+    static func < (lhs: Date, rhs: Date) -> Bool {
+        if lhs.year != rhs.year {
+            return lhs.year < rhs.year
+        }
+        if lhs.month != rhs.month {
+            return lhs.month < rhs.month
+        }
+        return lhs.day < rhs.day
+    }
+}
+
+struct Time: Comparable, Hashable {
+    let hour: Int
+    let minute: Int
+
+    static func == (lhs: Time, rhs: Time) -> Bool {
+        return lhs.hour == rhs.hour
+            && lhs.minute == rhs.minute
+    }
+
+    static func < (lhs: Time, rhs: Time) -> Bool {
+        if lhs.hour != rhs.hour {
+            return lhs.hour < rhs.hour
+        }
+        return lhs.minute < rhs.minute
+    }
+
+    static func + (value: Time, amount: Int) -> Time {
+        return Time(
+            hour: (value.hour + (amount / 60)) % 24,
+            minute: (value.minute + amount) % 60
+        )
+    }
+}
+
+struct Timestamp: Comparable, Hashable {
+    let date: Date
+    let time: Time
+
+    static func == (lhs: Timestamp, rhs: Timestamp) -> Bool {
+        return lhs.date == rhs.date
+            && lhs.time == rhs.time
+    }
+
+    static func < (lhs: Timestamp, rhs: Timestamp) -> Bool {
+        if lhs.date != rhs.date {
+            return lhs.date < rhs.date
+        }
+        return lhs.time < rhs.time
+    }
+}
+
+struct Record: Comparable {
+    let timestamp: Timestamp
+    let note: Note
+
+    static func == (lhs: Record, rhs: Record) -> Bool {
+        return lhs.timestamp == rhs.timestamp
+    }
+
+    static func < (lhs: Record, rhs: Record) -> Bool {
+        return lhs.timestamp < rhs.timestamp
+    }
+}
+
+enum Note {
+    case beginsShift(guardId: GuardId)
+    case fallsAsleep
+    case wakesUp
+}
+
+struct GuardAtTime: Hashable {
+    let guardId: GuardId
+    let time: Time
+}
+
+func main() {
+    var records = StdIn().map(parseRecord)
+    records.sort()
+
+    var asleep: [GuardAtTime: Int] = [:]
+    var currentGuardId: GuardId!
+    var timeFallenAsleep: Time?
+    for record in records {
+        switch record.note {
+        case let .beginsShift(guardId):
+            currentGuardId = guardId
+        case .fallsAsleep:
+            timeFallenAsleep = record.timestamp.time
+        case .wakesUp:
+            var time = timeFallenAsleep!
+            while time != record.timestamp.time {
+                let guardAtTime = GuardAtTime(guardId: currentGuardId, time: time)
+                asleep[guardAtTime] = (asleep[guardAtTime] ?? 0) + 1
+                time = time + 1
+            }
+            timeFallenAsleep = nil
+        }
+    }
+
+    let (selected, _) = asleep.max(by: comparing({ _, count in count }))!
+    let result = selected.guardId * selected.time.minute
+    print("\(selected.guardId) * \(selected.time.minute) = \(result)")
+}
+
+let timestampParser = "\\[(\\d+)-(\\d+)-(\\d+) (\\d+):(\\d+)\\]"
+let beginsShiftParser = try! RegExp(pattern: "^\(timestampParser) Guard #(\\d+) begins shift$")
+let fallsAsleepParser = try! RegExp(pattern: "^\(timestampParser) falls asleep$")
+let wakesUpParser = try! RegExp(pattern: "^\(timestampParser) wakes up$")
+
+func parseRecord(string: String) -> Record {
+    if let match = beginsShiftParser.firstMatch(in: string) {
+        return Record(
+            timestamp: extractTimestamp(match: match),
+            note: Note.beginsShift(guardId: GuardId(match[6])!)
+        )
+    }
+    if let match = fallsAsleepParser.firstMatch(in: string) {
+        return Record(
+            timestamp: extractTimestamp(match: match),
+            note: Note.fallsAsleep
+        )
+    }
+    if let match = wakesUpParser.firstMatch(in: string) {
+        return Record(
+            timestamp: extractTimestamp(match: match),
+            note: Note.wakesUp
+        )
+    }
+    fatalError("Could not parse: \(string)")
+}
+
+func extractTimestamp(match: RegExpMatch) -> Timestamp {
+    return Timestamp(
+        date: Date(
+            year: Int(match[1])!,
+            month: Int(match[2])!,
+            day: Int(match[3])!
+        ),
+        time: Time(
+            hour: Int(match[4])!,
+            minute: Int(match[5])!
+        )
+    )
+}
