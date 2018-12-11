e8e3fde19fb356821b0a3abe791c331191cf21b5
Samir Talwar <samir@noodlesandwich.com>
2018-12-11 18:10:26 +0000

    Rewrite 2018, Day 11, Exercise 2 using a summed-area table.
    https://en.wikipedia.org/wiki/Summed-area_table

--- 2018/AOC_11_2.swift
+++ 2018/AOC_11_2.swift
@@ -4,46 +4,31 @@ typealias SerialNumber = Int
 
 typealias PowerLevel = Int
 
-struct Cell: Hashable {
-    let x: Int
-    let y: Int
-
-    static func + (cell: Cell, offset: (Int, Int)) -> Cell {
-        return Cell(x: cell.x + offset.0, y: cell.y + offset.1)
-    }
-}
-
-struct Square: Hashable {
-    let corner: Cell
-    let size: Int
-
-    var bottomAndRightCells: [Cell] {
-        let max = size - 1
-        let bottomRightCorner = corner + (max, max)
-        let bottom = (0 ..< max).map({ x in self.corner + (x, max) })
-        let right = (0 ..< max).map({ y in self.corner + (max, y) })
-        return [bottomRightCorner] + bottom + right
-    }
-}
-
 func main() {
     let serialNumber = SerialNumber(readLine()!)!
 
-    let cells = (1 ... gridSize).flatMap({ y in (1 ... gridSize).map({ x in Cell(x: x, y: y) }) })
-    let powerLevels = Dictionary(
-        uniqueKeysWithValues: cells.map({ cell in
-            (cell, powerLevel(of: cell, serialNumber: serialNumber))
-        })
-    )
+    var summedArea = Array(repeating: Array(repeating: 0, count: gridSize + 1), count: gridSize + 1)
+    for y in 1 ... gridSize {
+        for x in 1 ... gridSize {
+            summedArea[y][x] =
+                powerLevel(x: x, y: y, serialNumber: serialNumber)
+                + summedArea[y][x - 1]
+                + summedArea[y - 1][x]
+                - summedArea[y - 1][x - 1]
+        }
+    }
 
-    var currentLargest: (Square, PowerLevel) = (Square(corner: Cell(x: 0, y: 0), size: 0), 0)
-    for cell in cells {
-        var powerLevel = 0
-        for size in 1 ... (gridSize - max(cell.x, cell.y) + 1) {
-            let square = Square(corner: cell, size: size)
-            powerLevel += square.bottomAndRightCells.map({ cell in powerLevels[cell]! }).reduce(0, +)
-            if powerLevel > currentLargest.1 {
-                currentLargest = (square, powerLevel)
+    var currentLargest: (Int, Int, Int, PowerLevel) = (0, 0, 0, 0)
+    for y in 1 ... gridSize {
+        for x in 1 ... gridSize {
+            for size in 1 ... (gridSize - max(x, y) + 1) {
+                let total = summedArea[y - 1][x - 1]
+                    - summedArea[y - 1][x - 1 + size]
+                    - summedArea[y - 1 + size][x - 1]
+                    + summedArea[y - 1 + size][x - 1 + size]
+                if total > currentLargest.3 {
+                    currentLargest = (x, y, size, total)
+                }
             }
         }
     }
@@ -51,9 +36,9 @@ func main() {
     print(currentLargest)
 }
 
-func powerLevel(of cell: Cell, serialNumber: SerialNumber) -> PowerLevel {
-    let rackId = cell.x + 10
-    var powerLevel = rackId * cell.y
+func powerLevel(x: Int, y: Int, serialNumber: SerialNumber) -> PowerLevel {
+    let rackId = x + 10
+    var powerLevel = rackId * y
     powerLevel += serialNumber
     powerLevel *= rackId
     let hundredsDigit = (powerLevel / 100) % 10
