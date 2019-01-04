65b68721aac1f62ad8273bc2e46d048e36fae5bb
Samir Talwar <samir@noodlesandwich.com>
2019-01-04 03:32:55 +0000

    2018: Extract out ElfCode instruction parsing.

--- 2018/AOC_19_1.swift
+++ 2018/AOC_19_1.swift
@@ -1,9 +1,6 @@
-let instructionPointerAssignmentParser = try! RegExp(pattern: "^#ip (\\d+)$")
-let instructionParser = try! RegExp(pattern: "^(\\w+) (\\d+) (\\d+) (\\d+)$")
-
 func main() {
-    let instructionPointerAddress = parseInstructionPointerAssignment(readLine()!)
-    let instructions = StdIn().map(parseInstruction)
+    let instructionPointerAddress = ElfCode.parseInstructionPointerAssignment(readLine()!)
+    let instructions = StdIn().map(ElfCode.parseInstruction)
 
     var registers = ElfCode.Registers(values: [0, 0, 0, 0, 0, 0])
     var instructionPointer = registers[instructionPointerAddress]
@@ -14,23 +11,4 @@ func main() {
         instructionPointer += 1
     }
     print(registers)
-}
-
-func parseInstructionPointerAssignment(_ string: String) -> ElfCode.Registers.Address {
-    guard let match = instructionPointerAssignmentParser.firstMatch(in: string) else {
-        fatalError("Could not parse the instruction pointer assignment:\n\"\(string)\"")
-    }
-    return ElfCode.Registers.Address(match[1])!
-}
-
-func parseInstruction(_ string: String) -> ElfCode.Instruction {
-    guard let match = instructionParser.firstMatch(in: string) else {
-        fatalError("Could not parse the instruction:\n\"\(string)\"")
-    }
-    return ElfCode.Instruction(
-        operation: ElfCode.Operation.value(of: String(match[1])),
-        inputA: ElfCode.Input(match[2])!,
-        inputB: ElfCode.Input(match[3])!,
-        output: ElfCode.Output(match[4])!
-    )
 }
--- 2018/Helpers/ElfCode.swift
+++ 2018/Helpers/ElfCode.swift
@@ -169,4 +169,26 @@ struct ElfCode {
             }
         }
     }
+
+    static let instructionPointerAssignmentParser = try! RegExp(pattern: "^#ip (\\d+)$")
+    static let instructionParser = try! RegExp(pattern: "^(\\w+) (\\d+) (\\d+) (\\d+)$")
+
+    static func parseInstructionPointerAssignment(_ string: String) -> Registers.Address {
+        guard let match = instructionPointerAssignmentParser.firstMatch(in: string) else {
+            fatalError("Could not parse the instruction pointer assignment:\n\"\(string)\"")
+        }
+        return Registers.Address(match[1])!
+    }
+
+    static func parseInstruction(_ string: String) -> Instruction {
+        guard let match = instructionParser.firstMatch(in: string) else {
+            fatalError("Could not parse the instruction:\n\"\(string)\"")
+        }
+        return Instruction(
+            operation: Operation.value(of: String(match[1])),
+            inputA: Input(match[2])!,
+            inputB: Input(match[3])!,
+            output: Output(match[4])!
+        )
+    }
 }
