3bb86c8ed7237b744f4f01d2d17995a56cdbcc74
Samir Talwar <samir@noodlesandwich.com>
2019-11-09 14:42:45 +0100

    Configure GHC and its dependencies with Nix.

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+let
+  ghc = haskellPackages.ghcWithPackages (
+    ps: with ps; [
+      aeson
+      array
+      base
+      bytestring
+      containers
+      cryptonite
+      hashmap
+      lens
+      parsec
+      scientific
+      text
+    ]
+  );
+in
+stdenv.mkDerivation {
+  name = "advent-of-code";
+
+  buildInputs = [
+    ghc
+  ];
+}
