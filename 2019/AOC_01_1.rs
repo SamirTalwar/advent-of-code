8b654462559b2cd120d67504e14f9f1982213a20
Samir Talwar <samir@noodlesandwich.com>
2019-12-04 17:54:29 +0100

    2019: Day 1, part 1.

--- 2019/AOC_01_1.rs
+++ 2019/AOC_01_1.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::BufRead;
+
+fn main() -> io::Result<()> {
+    let module_masses: Vec<i32> = io::BufReader::new(io::stdin())
+        .lines()
+        .map(|input| {
+            input.and_then(|line| {
+                line.parse::<i32>()
+                    .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+            })
+        })
+        .collect::<io::Result<_>>()?;
+
+    let fuel: i32 = module_masses.iter().map(|mass| calculate_fuel(mass)).sum();
+
+    println!("{}", fuel);
+
+    Ok(())
+}
+
+fn calculate_fuel(mass: &i32) -> i32 {
+    mass / 3 - 2
 }
