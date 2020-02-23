296e6d1b9564d14361d778b07482af0d593136dd
Samir Talwar <samir@noodlesandwich.com>
2020-02-23 18:43:47 +0100

    2019: Make `digits` generic over numeric types.

--- 2019/digits.rs
+++ 2019/digits.rs
@@ -1,30 +1,47 @@
+use std::cmp;
+
+use num;
+
 pub type Digit = u8;
 
 pub type Digits = Vec<Digit>;
 
-pub fn to_digits(number: u32) -> Digits {
-    if number == 0 {
+pub fn to_digits<N>(number: N) -> Digits
+where
+    N: num::Num + num::NumCast + num::ToPrimitive + cmp::PartialOrd + Copy,
+{
+    let zero: N = num::zero();
+    let ten: N = N::from(10).unwrap();
+
+    if number == zero {
         return vec![0];
     }
     let mut result = Vec::new();
     let mut current = number;
-    while current > 0 {
-        result.push((current % 10) as Digit);
-        current /= 10;
+    while current > zero {
+        result.push((current % ten).to_u8().unwrap());
+        current = current / ten;
     }
     result.reverse();
     result
 }
 
-pub fn from_digits(digits: Digits) -> u32 {
+pub fn from_digits<N>(digits: Digits) -> N
+where
+    N: num::Num + num::NumCast + Copy,
+{
+    let ten = N::from(10).unwrap();
     digits
         .iter()
-        .rfold((0u32, 1u32), |(accumulator, multiplier), digit| {
-            (
-                accumulator + u32::from(*digit) * multiplier,
-                multiplier * 10,
-            )
-        })
+        .rfold(
+            (num::zero(), num::one()),
+            |(accumulator, multiplier): (N, N), digit| {
+                (
+                    accumulator + N::from(*digit).unwrap() * multiplier,
+                    multiplier * ten,
+                )
+            },
+        )
         .0
         .into()
 }
