db83b59c330240f1b1b10f29140f4440025bd0ed
Samir Talwar <samir@noodlesandwich.com>
2021-12-02 06:24:34 +0100

    2021: Day 2, Part 2.

---
+++ 2021/AOC_02_2.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as IO
+import Debug.Trace (traceShowId)
+import Text.Parsec
+import Text.Parsec.Text
+
+data Command = Forward Int | Down Int | Up Int
+  deriving (Show)
+
+data Position = Position {horizontal :: Int, depth :: Int, aim :: Int}
+  deriving (Show)
+
+main = do
+  commands <- map parseInput . Text.lines <$> IO.getContents
+  let Position {horizontal, depth} = foldl move initialPosition commands
+  let answer = horizontal * depth
+  print answer
+
+initialPosition :: Position
+initialPosition = Position {horizontal = 0, depth = 0, aim = 0}
+
+move :: Position -> Command -> Position
+move position (Forward n) =
+  position
+    { horizontal = horizontal position + n,
+      depth = depth position + aim position * n
+    }
+move position (Up n) = position {aim = aim position - n}
+move position (Down n) = position {aim = aim position + n}
+
+parseInput :: Text -> Command
+parseInput = either (error . show) id . parse parser ""
+  where
+    parser = do
+      constructor <-
+        try (string "forward" >> pure Forward)
+          <|> try (string "down" >> pure Down)
+          <|> try (string "up" >> pure Up)
+      many1 space
+      amount <- read <$> many1 digit
+      return $ constructor amount
