9c1c9afbe0a61595f0e817536f399d0dbf78b6a0
Samir Talwar <samir@noodlesandwich.com>
2021-12-05 12:20:33 +0100

    2021: Use list comprehensions to make Day 5 prettier.

--- 2021/AOC_05_1.hs
+++ 2021/AOC_05_1.hs
@@ -1,7 +1,6 @@
 {-# OPTIONS -Wall #-}
 
 import qualified Data.Map as Map
-import Helpers.Applicative
 import Helpers.Map
 import Helpers.Parse
 import Text.Parsec hiding (Line)
@@ -15,22 +14,15 @@ data Line = Line Coordinate Coordinate
 main :: IO ()
 main = do
   input <- parseInput
-  let validLines = filter (isHorizontal <||> isVertical) input
-  let allCoordinates = concatMap allCoordinatesInLine validLines
+  let allCoordinates = concatMap allCoordinatesInLine input
   let counts = countValues allCoordinates
   print $ Map.size $ Map.filter (> 1) counts
 
 allCoordinatesInLine :: Line -> [Coordinate]
 allCoordinatesInLine (Line (Coordinate x1 y1) (Coordinate x2 y2))
-  | x1 == x2 = map (Coordinate x1) (if y1 <= y2 then [y1 .. y2] else [y2 .. y1])
-  | y1 == y2 = map (`Coordinate` y1) (if x1 <= x2 then [x1 .. x2] else [x2 .. x1])
-  | otherwise = error "Invalid line."
-
-isHorizontal :: Line -> Bool
-isHorizontal (Line (Coordinate x1 _) (Coordinate x2 _)) = x1 == x2
-
-isVertical :: Line -> Bool
-isVertical (Line (Coordinate _ y1) (Coordinate _ y2)) = y1 == y2
+  | x1 == x2 = [Coordinate x1 y | y <- if y1 <= y2 then [y1 .. y2] else [y2 .. y1]]
+  | y1 == y2 = [Coordinate x y1 | x <- if x1 <= x2 then [x1 .. x2] else [x2 .. x1]]
+  | otherwise = []
 
 parseInput :: IO [Line]
 parseInput = parseLinesIO line
--- 2021/AOC_05_2.hs
+++ 2021/AOC_05_2.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS -Wall #-}
+{-# LANGUAGE ParallelListComp #-}
 
 import qualified Data.Map as Map
 import Helpers.Map
@@ -20,12 +21,13 @@ main = do
 
 allCoordinatesInLine :: Line -> [Coordinate]
 allCoordinatesInLine (Line (Coordinate x1 y1) (Coordinate x2 y2))
-  | x1 == x2 = map (Coordinate x1) (if y1 <= y2 then [y1 .. y2] else [y2 .. y1])
-  | y1 == y2 = map (`Coordinate` y1) (if x1 <= x2 then [x1 .. x2] else [x2 .. x1])
+  | x1 == x2 = [Coordinate x1 y | y <- if y1 <= y2 then [y1 .. y2] else [y2 .. y1]]
+  | y1 == y2 = [Coordinate x y1 | x <- if x1 <= x2 then [x1 .. x2] else [x2 .. x1]]
   | otherwise =
-    let xs = if x1 <= x2 then [x1 .. x2] else reverse [x2 .. x1]
-        ys = if y1 <= y2 then [y1 .. y2] else reverse [y2 .. y1]
-     in zipWith Coordinate xs ys
+    [ Coordinate x y
+      | x <- if x1 <= x2 then [x1 .. x2] else reverse [x2 .. x1]
+      | y <- if y1 <= y2 then [y1 .. y2] else reverse [y2 .. y1]
+    ]
 
 parseInput :: IO [Line]
 parseInput = parseLinesIO line
