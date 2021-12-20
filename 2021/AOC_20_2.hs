1fc5b63a54b846972b4fc80b18cd4b464d2f930c
Samir Talwar <samir@noodlesandwich.com>
2021-12-20 19:40:40 +0100

    2021: Speed up Day 20 a little by using a `Set` instead of a `Grid`.
    We only care about Light pixels on Dark backgrounds, or vice versa.

--- 2021/AOC_20_2.hs
+++ 2021/AOC_20_2.hs
@@ -3,7 +3,6 @@
 import Control.Monad (when)
 import qualified Data.Foldable as Foldable
 import Data.Functor (($>))
-import qualified Data.Map.Strict as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Text (Text)
@@ -11,12 +10,10 @@ import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 import Data.Vector (Vector)
 import qualified Data.Vector as Vector
-import Helpers.Grid (Grid)
-import qualified Helpers.Grid as Grid
+import Helpers.Function
 import Helpers.Numbers (unBits)
 import Helpers.Parse
 import Helpers.Point (Point (..))
-import qualified Helpers.Point as Point
 import Text.Parsec
 
 data Pixel = Dark | Light
@@ -31,27 +28,34 @@ main = do
   input <- Text.lines <$> Text.IO.getContents
   let enhancement = Vector.fromList $ parseText parser $ head input
   when (Vector.length enhancement /= 512) $ fail $ "Invalid enhancement length of " <> show (Vector.length enhancement) <> ".''"
-  let image = Grid.toSparse Dark . Grid.fromList $ map (parseText parser) $ tail (tail input)
+  let pixels = map (parseText parser) $ tail (tail input)
+  let image = parseImagePoints pixels
   let steps = map fst $ iterate (step enhancement) (image, Dark)
-  print $ length $ Grid.allPointsList $ steps !! 50
+  print $ Set.size $ steps !! 50
 
-step :: Vector Pixel -> (Grid Pixel, Pixel) -> (Grid Pixel, Pixel)
+parseImagePoints :: [[Pixel]] -> Set Point
+parseImagePoints pixels =
+  pixels
+    |> zipWith (\y row -> zipWith (\x pixel -> (Point y x, pixel)) [0 ..] row) [0 ..]
+    |> concat
+    |> filter ((== Light) . snd)
+    |> map fst
+    |> Set.fromList
+
+step :: Vector Pixel -> (Set Point, Pixel) -> (Set Point, Pixel)
 step enhancement (image, base) =
-  (Grid.fromPoints newBase (Map.fromSet (const op) enhanced), newBase)
+  (enhanced, newBase)
   where
     newBase = case base of Dark -> Vector.head enhancement; Light -> Vector.last enhancement
     op = opposite newBase
-    points = Foldable.foldMap' expand $ Grid.allPoints image
-    enhanced = Set.filter (\p -> enhance enhancement image p == op) points
+    points = Foldable.foldMap' (Set.fromList . considered) image
+    enhanced = Set.filter (\p -> enhance enhancement base image p == op) points
 
-expand :: Point -> Set Point
-expand point = Set.insert point $ Point.neighboringPointsWithDiagonals point
-
-enhance :: Vector Pixel -> Grid Pixel -> Point -> Pixel
-enhance enhancement image point =
+enhance :: Vector Pixel -> Pixel -> Set Point -> Point -> Pixel
+enhance enhancement base image point =
   enhancement Vector.! pixelsToInt pixels
   where
-    pixels = map (image Grid.!) $ considered point
+    pixels = map (\p -> if p `Set.member` image then opposite base else base) (considered point)
 
 opposite :: Pixel -> Pixel
 opposite Light = Dark
