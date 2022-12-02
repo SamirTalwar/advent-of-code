477d824a2e91daea6e4d0f3f9664ad2304676912
Samir Talwar <samir@noodlesandwich.com>
2022-12-02 05:30:10 +0100

    2022: Day 1, part 2.

---
+++ 2022/AOC_01_2.sql
@@ -0,0 +1,32 @@
+CREATE TABLE input(
+  line SERIAL NOT NULL PRIMARY KEY,
+  calories INT NULL
+);
+INSERT INTO input VALUES (0, NULL); -- so we always get a match for constructing `input_elf`
+COPY input(calories) FROM :'input' WITH (FORMAT csv, HEADER false);
+
+CREATE MATERIALIZED VIEW input_elf AS
+     SELECT input.line, COUNT(grouping.*) AS elf
+       FROM input
+  LEFT JOIN input AS grouping ON grouping.line < input.line
+      WHERE input.calories IS NOT NULL
+        AND grouping.calories IS NULL
+   GROUP BY input.line
+   ORDER BY input.line;
+
+CREATE MATERIALIZED VIEW calories AS
+    SELECT elf, SUM(calories) AS total
+      FROM input
+      JOIN input_elf ON input.line = input_elf.line
+  GROUP BY elf;
+
+CREATE MATERIALIZED VIEW top_three AS
+    SELECT total
+      FROM calories
+  ORDER BY total DESC
+     FETCH FIRST 3 ROWS ONLY;
+
+\echo
+\t on
+\o
+SELECT SUM(total) FROM top_three;
