5832dbf3fa5d42e02d468bee5501e97cd16c113d
Samir Talwar <samir@noodlesandwich.com>
2022-12-06 12:30:21 +0100

    2022: Day 6, part 2.

---
+++ 2022/AOC_06_2.sql
@@ -0,0 +1,29 @@
+CREATE TABLE input(
+  datastream TEXT NOT NULL
+);
+COPY input(datastream) FROM :'input' WITH (FORMAT csv, HEADER false);
+
+CREATE TABLE data_values(
+  i SERIAL NOT NULL PRIMARY KEY,
+  c CHAR NOT NULL
+);
+INSERT INTO data_values(c) (SELECT string_to_table(datastream, NULL) FROM input);
+
+CREATE MATERIALIZED VIEW potential AS
+    SELECT initial.i, array_agg(values.c) segment
+      FROM data_values initial
+      JOIN data_values values
+           ON values.i BETWEEN initial.i AND initial.i + 13
+  GROUP BY initial.i;
+
+CREATE MATERIALIZED VIEW message AS
+  SELECT i AS message_start,
+         i + array_length(segment, 1) - 1 AS message_end
+    FROM potential
+   WHERE array_length(array(SELECT DISTINCT x FROM unnest(segment) x), 1) = array_length(segment, 1);
+
+\echo
+\t on
+\o
+
+SELECT MIN(message_end) FROM message;
