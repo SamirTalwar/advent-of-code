df9bb1453394c2b812391c980fc6b12bdf9d737b
Samir Talwar <samir@functional.computer>
2023-12-02 20:59:21 +0100

    2023: Set up a working Haskell shell and `run` script.

---
+++ 2023/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/28aec35166022d8ecde7d210912179ab226e2397.tar.gz") { } }:
+let
+  ghc = pkgs.haskellPackages.ghcWithPackages (
+    ps: with ps; [
+      base
+    ]
+  );
+in
+with pkgs;
+stdenv.mkDerivation {
+  name = "advent-of-code-2023";
+
+  buildInputs = [
+    ghc
+    haskell-language-server
+    hlint
+    ormolu
+  ];
+}
