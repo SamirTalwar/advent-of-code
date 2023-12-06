73d9b5f78c0bf8141ed3a1bbc5ac9f65b14d8f7a
Samir Talwar <samir@functional.computer>
2023-12-06 21:55:10 +0100

    2023: Day 6, part 2. Optimized by solving the quadratic equation.
    For some reason, this didn't work for part 1.
    
    Shrug.

--- 2023/AOC_06_2.hs
+++ 2023/AOC_06_2.hs
@@ -4,11 +4,17 @@ main = do
   let result = wins raceTime raceRecord
   print result
 
+-- solve for: x * (time - x) - record
+-- -x^2 + time * x - record
+-- x = (time ± sqrt(time^2 - 4 * record)) / 2
+-- we want the range between the two solutions, inclusive
 wins :: Int -> Int -> Int
 wins time record =
-  let lossTimes = takeWhile (<= record) $ map (\charge -> charge * (time - charge)) [0 .. time]
-      losses = length lossTimes * 2
-   in time - losses + 1
+  let a = floor $ (fromIntegral time + middle) / 2
+      b = ceiling $ (fromIntegral time - middle) / 2
+   in abs (a - b) + 1
+  where
+    middle :: Double = sqrt . fromIntegral $ time * time - 4 * record
 
 parse :: String -> Int
 parse input = read . concat . chunk $ dropWhile (/= ' ') input
