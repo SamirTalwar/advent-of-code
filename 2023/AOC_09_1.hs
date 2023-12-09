92508e397b5bd24da1ba36ae9464d900363bc3d8
Samir Talwar <samir@functional.computer>
2023-12-09 20:59:14 +0100

    2023: Day 9, part 1.

---
+++ 2023/AOC_09_1.hs
@@ -0,0 +1,20 @@
+main :: IO ()
+main = do
+  input <- map (map (read @Int) . words) . lines <$> getContents
+  let nexts = map computeNext input
+      result = sum nexts
+  print result
+
+computeNext :: [Int] -> Int
+computeNext values =
+  if all (== 0) values
+    then 0
+    else last values + computeNext differences
+  where
+    differences = map (uncurry (flip (-))) (pairs values)
+
+pairs :: [a] -> [(a, a)]
+pairs [] = []
+pairs [_] = []
+pairs [x, y] = [(x, y)]
+pairs (x : y : rest) = (x, y) : pairs (y : rest)
