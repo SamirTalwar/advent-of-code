6b42f42edabf8a08fd921ed51106eeac3b7d84b9
Samir Talwar <samir@functional.computer>
2024-12-02 10:08:18 +0100

    2024: Day 2, part 1.

---
+++ 2024/AOC_02_1.cs
@@ -0,0 +1,19 @@
+class AOC_02_1
+{
+    public static void Main(string[] args)
+    {
+        var inputs = Input.Lines().Select(line => line.Split().Select(int.Parse).ToList());
+
+        var result = inputs.Count(Solve);
+
+        Console.WriteLine("{0}", result);
+    }
+
+    static bool Solve(IList<int> input)
+    {
+        var differences = input.Zip(input.Skip(1), (a, b) => b - a);
+        var safelyIncreasing = differences.All(d => d >= 1 && d <= 3);
+        var safelyDecreasing = differences.All(d => d >= -3 && d <= -1);
+        return safelyIncreasing || safelyDecreasing;
+    }
+}
