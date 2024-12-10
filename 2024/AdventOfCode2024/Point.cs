9e7a196d6fe07b9ca36d26042bba989d41ff6eba
Samir Talwar <samir@functional.computer>
2024-12-10 20:47:19 +0100

    2024: Convert `Point` into a `record struct`.

--- 2024/AdventOfCode2024/Point.cs
+++ 2024/AdventOfCode2024/Point.cs
@@ -1,8 +1,5 @@
-public struct Point2D : IComparable
+public record struct Point2D(int Y, int X) : IComparable
 {
-    public int Y { get; init; }
-    public int X { get; init; }
-
     public static Point2D operator +(Point2D a, Point2D b) =>
         new Point2D { Y = a.Y + b.Y, X = a.X + b.X };
 
@@ -11,16 +8,6 @@ public struct Point2D : IComparable
 
     public static Point2D operator -(Point2D a, Point2D b) =>
         new Point2D { Y = a.Y - b.Y, X = a.X - b.X };
-
-    public override bool Equals(object? obj) => obj is Point2D other && this.Equals(other);
-
-    public bool Equals(Point2D p) => Y == p.Y && X == p.X;
-
-    public static bool operator ==(Point2D lhs, Point2D rhs) => lhs.Equals(rhs);
-
-    public static bool operator !=(Point2D lhs, Point2D rhs) => !lhs.Equals(rhs);
-
-    public override int GetHashCode() => (Y, X).GetHashCode();
 
     public int CompareTo(object? obj) => obj is Point2D other ? (this.Y, this.X).CompareTo((other.Y, other.X)) : 1;
 
